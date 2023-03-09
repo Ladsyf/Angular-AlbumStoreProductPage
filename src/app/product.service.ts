4766120606228d65041b173242a230bc2ba155e2
Ladsyf <99601710+Ladsyf@users.noreply.github.com>
2023-03-09 19:14:30 +0800

    Creating a Service to Get Album Data v3

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { map } from 'rxjs/operators';
+import { map } from 'rxjs/add/operators/map';
 import { Http, Response } from '@angular/http';
 
 @Injectable({
